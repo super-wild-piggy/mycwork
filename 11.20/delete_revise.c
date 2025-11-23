ef7a5ebc07143210740e71f85914f1ad9a4d9143
super-wild-piggy <1684588556@qq.com>
2025-11-23 21:23:01 +0800

    添加学习了双指针法后写的字符串去重

---
+++ 11.20/delete_revise.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[1000];
+    printf("请输入\n");
+    gets(str);
+    int len=strlen(str);
+    int i,j;
+    j=0;
+    for(i=1;i<len;i++){
+        if(str[j]!=str[i]){
+            j++;
+            str[j]=str[i];  //j用来记录数据走到哪里了
+        }
+    }
+    str[j+1]='\0';
+    puts(str);
+    return 0;
+}
+
+/*修改后的版本使用双指针法
+此方法为快慢指针法
+j是慢指针 用来记录位置
+i是快指针 用来遍历数组*/
