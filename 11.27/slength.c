721e57b75d86c98f4872233dc933297221db93d8
super-wild-piggy <1684588556@qq.com>
2025-11-27 14:57:22 +0800

    添加修改后的版本+心得

--- 11.27/slength.c
+++ 11.27/slength.c
@@ -8,12 +8,13 @@ int main(){
     int length=strlen(s);
     int i=0,j,maxlen=1,len=1,k;
     for(j=1;j<length;j++){
-        if(s[j]!=s[i]){
-            len++;
-        }else{
-            i++;
-            len--;
+        for(k=i;k<j;k++){
+            if(s[k]==s[j]){
+                i=k+1;
+                break;
+            }
         }
+        len=j-i+1;
         if(maxlen<len){
             maxlen=len;
         }
@@ -21,3 +22,8 @@ int main(){
     printf("%d",maxlen);
     return 0;
 }
+
+/*第一次提交发现我只检查了边界的元素是否重复 没有检查字符串内部
+窗口的大小可以用右边界-左边界+1 单纯的++浪费时间
+左边界可以在发现重复时直接挪到重复值的右侧 不用一直++
+使用双指针法时 在循环判断条件中的那个指针在循环内部不要改变值*/
