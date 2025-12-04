5e702ce6a30bdb3a9d3eb4d8e53649d65c4eeaff
super-wild-piggy <1684588556@qq.com>
2025-12-04 18:47:13 +0800

    修改

--- test/password.c
+++ test/password.c
@@ -20,6 +20,7 @@ int main(){
                     key[k]=key[k+1];
                 }
                 count--;
+                j--;
             }
         }
     }
@@ -34,16 +35,19 @@ int main(){
         reversekey[i]=standard;
         standard--;
     }
-    reversekey[i+1]='\0';
+    reversekey[i]='\0';
     for(i=0;i<count+26;i++){
-        for(j=i+1;j<count+26;j++){
+        for(j=i+1;j<count+26-1;j++){
             if(reversekey[i]==reversekey[j]){
                 for(int k=j;k<count+26;k++){
                     reversekey[k]=reversekey[k+1];
                 }
+                count--;
+                j--;
             }
         }
     }
+    reversekey[26]='\0';
     char str[1000];
     i=0;
     while((ch=getchar())!='\n'){
@@ -62,9 +66,16 @@ int main(){
             turned_str[i]=str[i];
         }
     }
+    turned_str[i]='\0';
     printf("%s",turned_str);
     return 0;
 }
 
 /*我的双指针法无法解决隔项去重
 疑问：为什么之前的key使用scanf读入 后面用getchar就不行了*/
+
+//根据ai提示更正 当删除一个重复字符后 j应该保持不变 因为后面的字符前移了
+//ai回答我的问题：因为用scanf读取密钥时 会在缓冲区里留下一个换行符
+//而getchar又会读取到这个换行符 导致提前结束
+
+//第二次发现问题 在for循环后我想要给最后一位赋'\0'但是数组越界了
